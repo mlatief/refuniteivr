abd2cbe01b051374aa600442b1ce1ead97deaa5c
Mohamed El Ersh <mohamed.elersh@tahrirtech.com>
2013-06-01 13:06:08 +0200

    Create Profile Implemented

--- refunite/api.py
+++ refunite/api.py
@@ -6,10 +6,45 @@ Created on May 31, 2013
 
 '''
 import urllib2
+import urllib
+
 import json
 import logging
 
 logging.getLogger().setLevel(logging.DEBUG)
+
+def createProfile (genderID,name,tribe,birthCountryID,MSISDN):
+    "Create New Refugee Profile."
+#    Call_API('/profile.json');    
+    query_args= {'genderId':genderID,'givenName':name,'tribe':tribe,'birthCountryId':birthCountryID,'cellPhone':MSISDN};
+    return post_API('/profile', query_args);
+
+def post_API(inputStr, args):
+    logging.log(logging.INFO, "Post API");
+    URL = 'http://api.ru.istykker.dk' + inputStr + '.json';
+
+    Realm    = 'api.ru.istykker.dk';
+    Username = 'hackathon';
+    Password = '473ba3ff6162c6064479825bcefa7de95e2ef266';
+    
+    logging.log(logging.INFO, URL);
+    
+    #Call API and log the return values.
+    authhandler = urllib2.HTTPDigestAuthHandler();
+    authhandler.add_password(Realm, URL, Username, Password);
+    opener = urllib2.build_opener(authhandler);
+    urllib2.install_opener(opener);
+    #params = urllib.urlencode(args);
+    
+    logging.log(logging.INFO, args);
+    
+    req = urllib2.Request(URL, str(args));
+    resp = urllib2.urlopen(req);
+    resp_json = json.loads(resp.read());
+    logging.log(logging.INFO, "REST Return Result ");
+    logging.log(logging.INFO, resp_json)
+    return resp_json;
+
 
 def Call_API(inputStr):
     "Utility function for calling REST APIs from Refugee United Servers."
@@ -18,7 +53,7 @@ def Call_API(inputStr):
     #RESTCall ='/partner.json'
     logging.log(logging.INFO, "call_api");
     # URL String Creation.
-    URL = 'http://api.ru.istykker.dk' + inputStr;
+    URL = 'http://api.ru.istykker.dk' + inputStr + ".json";
     logging.log (logging.INFO, "URL REST ---> #" + URL+"#");
     # REST API Configurations.
     Realm    = 'api.ru.istykker.dk';
@@ -37,9 +72,17 @@ def Call_API(inputStr):
 
 def getOwningVolunteer(refugeeID):
     "This returns ID of owning volunteer associated with passed Refugee ID"
-    X = Call_API("/profile/" + str(refugeeID)+".json");
+    X = Call_API("/profile/" + str(refugeeID));
+    logging.log(logging.INFO, X.get("profile").get("genderId"))
     logging.log(logging.INFO, X.get("profile").get("genderId"))
     return;
+
+def search(name, genderId, countryOfBirthId):
+    "Calls search REST API with Parameters passed."
+    logMeIn = "Search for -- > name:" + name + ",genderID:" + genderId + ",Refugee Country:" + countryOfBirthId+".";    
+    logging.log(logging.INFO, logMeIn);  
+    return;
+
 
 ''''
 Functions::
@@ -59,29 +102,11 @@ getOwningVolunteer
 Admin Panel
 
 '''
-'''
-#  Fetch json
-RESTCall = '/search.json?name=Ali'
-#RESTCall ='/partner.json'
-URL = 'http://api.ru.istykker.dk' + RESTCall  #URL      = 'http://api.ru.istykker.dk/topcountries.json'
-Realm    = 'api.ru.istykker.dk'
-Username = 'hackathon'
-Password = '473ba3ff6162c6064479825bcefa7de95e2ef266'
 
-authhandler = urllib2.HTTPDigestAuthHandler()
-authhandler.add_password(Realm, URL, Username, Password)
-opener = urllib2.build_opener(authhandler)
-urllib2.install_opener(opener)
-page_content = urllib2.urlopen(URL)
+getOwningVolunteer("354587")
+search("Mohamed","1","SO")
+##XX = createProfile ("2","Marwa","XXX",'SO',"0212001311");
 
-response = json.loads(page_content.read())
-print response['count']
 
-def search():
-    print "XXXXXX"
-    return 0;
 
-'''
-
-#print ("start here")
-getOwningVolunteer("354544")
+#createProfile (genderID,name,tribe,birthCountryID,MSISDN);
